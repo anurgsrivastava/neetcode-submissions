d44a663d3044645bc618a49dbf74158e97d71d87
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-01 20:29:55 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.ts
@@ -0,0 +1,63 @@
+class MyStack {
+    queue1;
+    queue2;
+
+    constructor() {
+        this.queue1 = [];
+        this.queue2 = [];
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x: number): void {
+        this.queue1.push(x);
+        console.log('push', this.queue1);
+    }
+
+    /**
+     * @return {number}
+     */
+    pop(): number {
+        if(!this.queue1.length) {
+            return -1;
+        }
+        let res = this.queue1[this.queue1.length - 1];
+        for(let i = 0; i < this.queue1.length - 1; i++) {
+            this.queue2.push(this.queue1[i]);
+        }
+        this.queue1 = this.queue2;
+        this.queue2 = [];
+        console.log('pop', this.queue1, res);
+        return res;
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        if(!this.queue1.length) {
+            return -1;
+        }
+        console.log('top', this.queue1, this.queue1[this.queue1.length - 1]);
+        return this.queue1[this.queue1.length - 1];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty(): boolean {
+        console.log('empty', this.queue1, this.queue1.length);
+        return !(this.queue1.length > 0);
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
