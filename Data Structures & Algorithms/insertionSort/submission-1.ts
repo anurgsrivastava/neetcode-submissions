6449c0fc194185ae912670d4204ffd87443d86ba
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-02 21:56:33 +0530

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.ts
@@ -0,0 +1,39 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs: Pair[]): Pair[][] {
+        if(!pairs?.length) {
+            return [];
+        }
+        let resPair = [];
+        resPair.push([...pairs]);
+
+        for (let i = 1; i < pairs.length; i++) {
+            let j = i;
+            while (j > 0 && pairs[j].key < pairs[j - 1].key) {
+                let temp = pairs[j];
+                pairs[j] = pairs[j - 1];
+                pairs[j - 1] = temp;
+                j -= 1;
+            }
+            resPair.push([...pairs]);
+        }
+
+        return resPair;
+    }
+}
