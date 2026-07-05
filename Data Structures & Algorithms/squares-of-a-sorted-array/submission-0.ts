612c49f9adc3c1b50d18e61a0efb306629323b3b
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-05 20:27:27 +0530

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortedSquares(nums: number[]): number[] {
+        nums.sort((a, b) => Math.abs(a) - Math.abs(b));
+
+        for(let i = 0; i < nums.length; i++) {
+            nums[i] = Math.pow(nums[i], 2); 
+        }
+
+        return nums;
+
+    }
+}
