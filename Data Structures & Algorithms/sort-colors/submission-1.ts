7cb187517e08526c2e19bcc6c4135174da0a35d3
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-06 01:50:01 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums: number[]): void {
+        let i = 0,
+            l = 0,
+            r = nums.length - 1;
+        while (i <= r) { 
+            if (nums[i] == 0) {
+                [nums[l], nums[i]] = [nums[i], nums[l]];
+                l++;
+            } else if (nums[i] == 2) {
+                [nums[i], nums[r]] = [nums[r], nums[i]];
+                r--;
+                i--;
+            }
+            i++;
+        }
+    }
+}
