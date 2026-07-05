6bbb0d599511a1c27d35af253f85d1ca03b48de7
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-05 23:11:00 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    decodeString(s) {
+        const stringStack = [];
+        const countStack = [];
+        let cur = '';
+        let k = 0;
+
+        for(const c of s) {
+            if (!isNaN(c)) {
+                k = k * 10 + parseInt(c, 10);
+            } else if (c === '[') {
+                stringStack.push(cur);
+                countStack.push(k);
+                cur = '';
+                k = 0;
+            } else if (c === ']') {
+                const temp = cur;
+                cur = stringStack.pop();
+                const count = countStack.pop();
+                cur += temp.repeat(count);
+            } else {
+                cur += c;
+            }
+        }
+        return cur;
+    }
+}
