d78b8292dc3574ebd56f6678630631b7919adced
Anurag Srivastava <srivastavaanurag20@gmail.com>
2026-07-06 10:30:52 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {character[]} tasks
+     * @param {number} n
+     * @return {number}
+     */
+    leastInterval(tasks: string[], n: number): number {
+        let count = new Array(26).fill(0);
+        for (let task of tasks) {
+            count[task.charCodeAt(0) - 'A'.charCodeAt(0)]++;
+        }
+        count.sort((a, b) => a - b);
+        const maxf = count[25];
+        let idle = (maxf - 1) * n;
+        for (let i = 24; i >= 0; i--) {
+            idle -= Math.min(maxf - 1, count[i]);
+        }
+        return Math.max(0, idle) + tasks.length;
+    }
+}
